7867d3ed6ddc92d70f5ca51dcc6c31b8c58f33de
Kennedy Cambra-Cho <kennedy.cambracho@gmail.com>
2019-09-09 16:52:37 +0800

    test1

--- WEEK2/IN-CLASS/WORDS/JS/script.js
+++ WEEK2/IN-CLASS/WORDS/JS/script.js
@@ -1,7 +1,17 @@
 function setup() {
+  createCanvas(600, 450);
+  background(240);
+  noStroke();
+  fill(40);
 
+
+  translate(20, 20);
+  rect(50, 50, 50, 50);
+  noStroke();
+  fill(200);
 }
 
 function draw() {
+
 
 }
