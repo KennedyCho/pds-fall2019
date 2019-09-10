7a4c99498da476a63cdfe6ebfeddf9750edc7bbb
Kennedy Cambra-Cho <kennedy.cambracho@gmail.com>
2019-09-10 19:37:01 +0800

    Final Word exercise

--- WEEK2/IN-CLASS/WORDS/JS/script.js
+++ WEEK2/IN-CLASS/WORDS/JS/script.js
@@ -1,16 +1,21 @@
+let x, y;
+
 function setup() {
-
-  createCanvas(600, 450);
-  background(240);
-  noStroke();
-
-
-
+  createCanvas(400, 400);
+  // Starts in the middle
+  x = width / 2;
+  y = height;
 }
 
 function draw() {
+  translate(-50, 50);
+  background(200);
 
-  fill('random');
-  translate(20, 20);
-  rect(random, random, 200, 200);
+  // Draw a rectangle
+  noStroke();
+  fill(0, 102, 204);
+  rect( x+130, 230, 100, 100);
+
+  // shiver
+  x = x + random(-1, 1);
 }
