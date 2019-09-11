471bb70a1b0f0823c3db06460fc3f4162987f6ef
Kennedy Cambra-Cho <kennedy.cambracho@gmail.com>
2019-09-11 16:49:00 +0800

    exercise 1

--- WEEK2/IN-CLASS/CUSTOM_SHAPES/JS/script.js
+++ WEEK2/IN-CLASS/CUSTOM_SHAPES/JS/script.js
@@ -1,21 +1,17 @@
-let x, y;
-
 function setup() {
   createCanvas(400, 400);
-  // Starts in the middle
-  x = width / 2;
-  y = height;
 }
 
 function draw() {
-  translate(-50, 50);
-  background(200);
+  background(220);
+  strokeWeight(5);
 
-  // Draw a rectangle
-  noStroke();
-  fill(0, 102, 204);
-  rect( x+130, 230, 100, 100);
 
-  // shiver
-  x = x + random(-2, 2);
+noStroke();
+//strokeWeight(1);
+  fill(155,0,0);
+beginShape();
+vertex(0, width/2);
+quadraticVertex(width/2, -width/2, width, width/2);
+endShape();
 }
