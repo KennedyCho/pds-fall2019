7110979d2d6760e939bb5e4312a82719078a0a00
Kennedy Cambra-Cho <kennedy.cambracho@gmail.com>
2019-09-11 16:40:32 +0800

    in-class custom shapes set up

---
+++ WEEK2/IN-CLASS/CUSTOM_SHAPES/JS/script.js
@@ -0,0 +1,21 @@
+let x, y;
+
+function setup() {
+  createCanvas(400, 400);
+  // Starts in the middle
+  x = width / 2;
+  y = height;
+}
+
+function draw() {
+  translate(-50, 50);
+  background(200);
+
+  // Draw a rectangle
+  noStroke();
+  fill(0, 102, 204);
+  rect( x+130, 230, 100, 100);
+
+  // shiver
+  x = x + random(-2, 2);
+}
