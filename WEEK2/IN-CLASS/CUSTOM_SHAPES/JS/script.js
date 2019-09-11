ade8d74a034f85a5b305349960a53794a384452b
Kennedy Cambra-Cho <kennedy.cambracho@gmail.com>
2019-09-11 17:07:28 +0800

    trial shapes

--- WEEK2/IN-CLASS/CUSTOM_SHAPES/JS/script.js
+++ WEEK2/IN-CLASS/CUSTOM_SHAPES/JS/script.js
@@ -1,5 +1,16 @@
+
 function setup() {
-  createCanvas(400, 400);
+
+  let canvasWidth = 400;
+  let canvasHeight = 400;
+
+  createCanvas(canvasWidth, canvasHeight);
+
+  // for (i = 0; i < canvasWidth; i+=10){
+  //   line(i, 0, i, canvasHeight);
+
+
+  }
 }
 
 function draw() {
@@ -7,11 +18,49 @@ function draw() {
   strokeWeight(5);
 
 
+
 noStroke();
 //strokeWeight(1);
   fill(155,0,0);
-beginShape();
-vertex(0, width/2);
-quadraticVertex(width/2, -width/2, width, width/2);
-endShape();
+
+  // //custom shape (basic arc) with quadraticVertex
+  // beginShape();
+  // // vertex(x, y)
+  // // y = first anchor point
+  // vertex(0, width/2);
+  //
+  // //quadraticVertex(a, b, c, d)
+  // // a,b (x,y) = control point (@ the peak of the arc)
+  // // c,d (x,y) = ending anchor point
+  // quadraticVertex(width/2, -width/2, width, width/2);
+  // endShape();
+
+  // //personal example: upside down arc
+  // beginShape();
+  // vertex(width/4, width/4);
+  // quadraticVertex(width/2, height, width - width/4, width/4);
+  // endShape();
+
+
+  // //custom shape with beziervertex
+  // noFill();
+  // beginShape();
+  // fill(155,0,0);
+  // // vertex(x, y)
+  // // y = first anchor point
+  // vertex(0, 200);
+  // // in order: control point 1, control point 2, ending anchor point
+  // bezierVertex(0, 200, 200, -200, 400, 200);
+  // endShape();
+
+  noFill();
+  beginShape();
+  fill(155, 0, 0);
+  vertex(50, 50);
+  bezierVertex(100, 200, 200, 300, 400, 200);
+  endShape();
+
+
+
+
 }
