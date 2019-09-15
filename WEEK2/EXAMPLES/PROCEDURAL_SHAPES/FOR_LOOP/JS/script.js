bcc6dd8587b387a0a66ff5ee6783b6b882d4707d
Kennedy Cambra-Cho <kennedy.cambracho@gmail.com>
2019-09-15 16:53:25 +0800

    SET UP

--- WEEK2/EXAMPLES/PROCEDURAL_SHAPES/FOR_LOOP/JS/script.js
+++ WEEK2/EXAMPLES/PROCEDURAL_SHAPES/FOR_LOOP/JS/script.js
@@ -1,37 +1,21 @@
 
 
 function setup() {
+  createCanvas(600, 500);
+  fill(0);
 
-  // lesser than sign
-  createCanvas(500, 500);
-    background(240);
+  for(var i = 0; i < 10; i++) {
+    rect(i * 105, 0, 100, 100);
+  }
 
-    var w = width * 0.35;
-    var h = width * 0.5;
-
-    // causes the shape to be cut along the borders of Shape
-    // without causes the inner shevron to be filled a white
-    noFill();
-    // darker < 40 < lighter
-    // 0 = black, 255 = white
-    stroke(0);
-    // Sets the style for rendering line endings
-    // SQUARE, PROJECT, and ROUND. The default cap is ROUND
-    strokeCap(SQUARE);
-    // makes the shape grow or shrink according to canvas width
-    strokeWeight(width * 0.08);
-    // start drawing at different point
-    translate((width/2 - (w/2)) , (height/2 - (h/2)) );
-    //
-    beginShape();
-    vertex(0, 0);
-    vertex(w, h/2);
-    vertex(0, h);
-    endShape();
-
-// I don't understand why the noLoop() is used
-    noLoop();
-
-
+  translate(width/2, height/2);
+  
+  beginShape();
+  for(var i = 0; i < 10; i++) {
+    var x = random(-100, 100);
+    var y = random(-100, 100);
+    vertex(x, y);
+  }
+  endShape();
 
 }
