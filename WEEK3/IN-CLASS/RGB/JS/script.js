33c71303f3d6840fb0e72321c525c93e2d66f7c4
Kennedy Cambra-Cho <kennedy.cambracho@gmail.com>
2019-09-17 14:21:41 +0800

    In-class exercise

--- WEEK3/IN-CLASS/RGB/JS/script.js
+++ WEEK3/IN-CLASS/RGB/JS/script.js
@@ -1,11 +1,26 @@
 
 function setup() {
-  createCanvas(400, 400);
 
-  noStroke();
 
-  colorMode(HSB, 100, 100, 100);
-  fill(90, 100, 100);
-  
+  createCanvas(500, 500);
+
+  var w = width * 0.5;
+  var h = height * 0.5;
+
+  stroke('red');
+  rect(0, 0, width, height);
+
+
+  for (i=0; i < 5; i++){
+
+    noStroke();
+
+    colorMode(HSB, 100, 100, 100);
+    fill(90, 100, 100);
+    ellipse(w+(i*2), h+(i*2), 200, 200)
+  }
+
+
+
 
 }
