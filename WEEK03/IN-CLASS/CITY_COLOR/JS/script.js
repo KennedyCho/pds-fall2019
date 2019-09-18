97e8f7efdb51cae5c2c5c96a0d6a4f422efa466e
Kennedy Cambra-Cho <kennedy.cambracho@gmail.com>
2019-09-18 17:07:31 +0800

    finish in-class exercise

--- WEEK03/IN-CLASS/CITY_COLOR/JS/script.js
+++ WEEK03/IN-CLASS/CITY_COLOR/JS/script.js
@@ -1,3 +1,23 @@
 function setup() {
+  //SINGAPORE
+
+  createCanvas(600, 600);
+  colorMode(RGB)
+
+  background(224, 227, 218);
+  let w = width;
+  let h = height;
+
+  noStroke();
+  fill(160, 13, 36);
+  rect(width/4, height/4, 100, 200);
+  noStroke();
+
+  fill(226, 118, 49);
+  rect(2 * width/4, height/2, 100, 200);
+  noStroke();
+
+  fill(109, 139, 171);
+  rect(3 * width/4, 3* height/4, 100, 200);
 
 }
