af94c51ff4f16304cfb3ce24196cc1ca0003c145
Kennedy Cambra-Cho <kennedy.cambracho@gmail.com>
2019-09-19 21:18:31 +0800

    water balloon + puncture

--- WEEK02/OPPOSITE_DESIGN/JS/script.js
+++ WEEK02/OPPOSITE_DESIGN/JS/script.js
@@ -2,25 +2,45 @@ function setup() {
   createCanvas(500, 500);
   background(0);
 
-  var w = width * 0.8;
-  var h = height * 0.8;
+  var w = width;
+  var h = height;
 
   var w2 = width * 0.3;
   var h2 = height * 0.3;
 
-  fill(255);
-  noStroke();
+  // balloon top
+  stroke('red');
   beginShape();
-    vertex(width, 0);
-    vertex(0, height);
-    vertex(width, h);
-    //vertex(0, h);
-    beginContour();
-      vertex(w2 * 0.3, h2 * 0.3);
-      vertex(w2 * 0.5, h2 * 0.8);
-      vertex(w2 * 0.8, h2 * 0.4);
-    endContour();
+    vertex(w*1/10, h*3/10);
+    vertex(w*2/10, h*2/10);
+    vertex(w*2/10, h*3/10);
+  endShape(CLOSE)
+
+  // balloon
+  stroke('blue');
+  beginShape();
+    vertex(w*2/10, h*3/10);
+    bezierVertex(w*2/10, h*5/10, w*2/10, h*9/10, w*6/10, h*6/10);
+    bezierVertex(w*8/10, h*4/10, w*5/10, h*3/10, w*2/10, h*3/10);
+  endShape(CLOSE)
+
+  //water
+  stroke('green');
+  beginShape();
+    fill(0);
+    vertex(w*5.3/10,h*2.9/10);
+    vertex(w*5.1/10,h*4/10);
+    vertex(w*5.5/10,h*3.5/10);
+    vertex(w*5.5/10,h*4.2/10);
+    vertex(w*5.9/10,h*3.8/10);
+    vertex(w*5.7/10,h*4.5/10);
+    vertex(w*6.4/10,h*4.2/10);
+    vertex(w*5.8/10,h*5/10);
+    vertex(w*8/10,h*4.6/10);
+
   endShape();
+
+
 
   noLoop();
 
