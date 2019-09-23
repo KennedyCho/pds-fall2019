ad76bae83dd19e9d6e9b0f4a8d6970368b992c93
Kennedy Cambra-Cho <kennedy.cambracho@gmail.com>
2019-09-23 11:09:33 +0800

    working doc

--- WEEK03/AMAZON_FIRE/JS/script.js
+++ WEEK03/AMAZON_FIRE/JS/script.js
@@ -1,12 +1,17 @@
 function setup() {
   createCanvas(600, 600);
-  background(255);
+  background(0);
+
+  let w = width;
+  let h = height;
 
   stroke('red');
+  strokeWeight(5);
+  strokeCap(ROUND);
   beginShape();
-    vertex(w*1/10, h*3/10);
-    vertex(w*2/10, h*2/10);
-    vertex(w*2/10, h*3/10);
+    vertex(w*1/10, h*1/10);
+    bezierVertex(w*2/10, h*5/10, w*3/10, h*4/10, w*4/10, h*3/10, w*6/10, h*5/10, w*9/10, h*9/10);
+
   endShape(CLOSE)
 
 }
