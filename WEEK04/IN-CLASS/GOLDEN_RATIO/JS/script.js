0880e9991ca4f13e4d2bd0e4c7f6f300dc637e08
Kennedy Cambra-Cho <kennedy.cambracho@gmail.com>
2019-10-06 23:24:07 +0800

    finish golden ration assignment

--- WEEK04/IN-CLASS/GOLDEN_RATIO/JS/script.js
+++ WEEK04/IN-CLASS/GOLDEN_RATIO/JS/script.js
@@ -1,11 +1,35 @@
-let goldenRatio = 1.61803398875;
 
-let w = width;
-let h = height;
+
 
 function setup() {
-  createCanvas(600, 600);
 
-  stroke('black');
-  line(0,20, w, 20);
+  createCanvas(600, 600);
+  background(0);
+
+  let goldenRatio = 1.61803398875;
+  let w = width;
+  // width = a + b
+  // (a+b)/GR = a
+  // (a+b) - a = b
+
+  // console.log(a);
+  // console.log(b);
+  // console.log(a+b);
+  var a = width/goldenRatio;
+  var b = width - a;
+
+  for (i=0; i<5; i++){
+
+
+    stroke(255);
+    strokeWeight(5);
+    line(a, 0, a, height);
+    line(0, b, width, b);
+    var b = a;
+    var a = a/goldenRatio;
+    var b = b - a;
+
+
+
+  }
 }
