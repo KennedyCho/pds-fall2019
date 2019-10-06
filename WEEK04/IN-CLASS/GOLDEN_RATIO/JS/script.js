fb585f14085ee775ee7694bfd42752e75f8cf4b6
Kennedy Cambra-Cho <kennedy.cambracho@gmail.com>
2019-10-06 22:42:52 +0800

    start assignment

--- WEEK04/IN-CLASS/GOLDEN_RATIO/JS/script.js
+++ WEEK04/IN-CLASS/GOLDEN_RATIO/JS/script.js
@@ -1,54 +1,11 @@
-// var size;
-//
-// function setup() {
-//   createCanvas(400, 300);
-//   size = 200;
-// }
-//
-// function draw() {
-//   background(220);
-//
-//   fill(255);
-//
-//   startX = 10;
-//   startY = 10;
-//
-//   translate(startX, startY);
-//
-//   var strokeW = 2;
-//
-//   for (var i = 0; i < 50; i++) {
-//     strokeW += 2;
-//     strokeWeight(strokeW);
-//
-//     drawSquare(size);
-//
-//     rotate(HALF_PI);
-//     scale(0.618);
-//     translate(0, -2.618 * size);
-//   }
-// }
-//
-// function drawSquare(size) {
-//   push();
-//   push();
-//   // noStroke();
-//   rect(0, 0, size, size);
-//   pop();
-//   noFill();
-//
-//   arc(size, size, 2 * size, 2 * size, PI, PI + HALF_PI);
-//   pop();
-// }
+let goldenRatio = 1.61803398875;
 
-
-
-
+let w = width;
+let h = height;
 
 function setup() {
-  createCanvas(600, 400);
-  background('grey');
-  let x = width;
-  let y = height;
-  line(y, 0, y, y);
+  createCanvas(600, 600);
+
+  stroke('black');
+  line(0,20, w, 20);
 }
