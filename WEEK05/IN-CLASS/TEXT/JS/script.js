75f6a20134aa9583f4f3ddf33f9545d581bd2f91
Kennedy Cambra-Cho <kennedy.cambracho@gmail.com>
2019-10-07 17:21:20 +0800

    set-up

---
+++ WEEK05/IN-CLASS/TEXT/JS/script.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(400, 400);
+
+}
+
+function draw() {
+  background(220);
+  fill(0);
+textSize(20);
+textFont('Georgia');
+text('Georgia', 12, 30);
+textFont('Helvetica');
+text('Helvetica', 12, 60);
+textFont('Georgia');
+text('Georgia', 12, 90);
+textFont('Helvetica');
+text('Helvetica', 12, 120);
+}
