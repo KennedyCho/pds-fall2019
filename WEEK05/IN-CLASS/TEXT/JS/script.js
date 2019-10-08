be54b8472a7e7b6a22984c3966e53f2ab315ad90
Kennedy Cambra-Cho <kennedy.cambracho@gmail.com>
2019-10-08 13:46:45 +0800

    poem

--- WEEK05/IN-CLASS/TEXT/JS/script.js
+++ WEEK05/IN-CLASS/TEXT/JS/script.js
@@ -1,18 +1,13 @@
 function setup() {
   createCanvas(400, 400);
-
 }
 
 function draw() {
   background(220);
-  fill(0);
-textSize(20);
-textFont('Georgia');
-text('Georgia', 12, 30);
-textFont('Helvetica');
-text('Helvetica', 12, 60);
-textFont('Georgia');
-text('Georgia', 12, 90);
-textFont('Helvetica');
-text('Helvetica', 12, 120);
+  let poem = ['he was', 'BISCUITS', 'but I was', 'not', 'gravy'];
+
+  fill(50);
+  textSize(30);
+  textFont('Helvetica');
+  text(poem, 10, 10, 170, 200); // Text wraps within text box
 }
