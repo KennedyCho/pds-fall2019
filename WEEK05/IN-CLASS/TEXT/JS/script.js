b58ee5133ee82d3bdbddbe7107d6fdfd4b1538d1
Kennedy Cambra-Cho <kennedy.cambracho@gmail.com>
2019-10-09 10:29:13 +0800

    finish peom assign.

--- WEEK05/IN-CLASS/TEXT/JS/script.js
+++ WEEK05/IN-CLASS/TEXT/JS/script.js
@@ -1,33 +1,43 @@
+let font
+fontsize = 40;
+
+function preload(){
+font = loadFont('ASSETS/ComicNeueSansID.ttf');
+}
 function setup() {
   createCanvas(600, 600);
+  textFont(font);
+  fill(50);
+  textSize(60);
 }
 
 function draw() {
-  background(220);
-  let line1 = 'he was biscuits';
+
+  background(255);
+  let line1 = 'he was ';
+  let bis = 'biscuits';
   let line2 = 'but I was';
   let line3 = 'not';
   let line4 = 'gravy';
 
-  fill(50);
-  textSize(60);
-  textFont('Helvetica');
-  text(line1, width/9,  height/6, 600, 100); // Text wraps within text box
+  text(line1, width/9,  height/6, 600, 100);
 
-  fill(50);
+  textSize(80);
+  text(bis, width/3 + width/6, height/8, 600, 100);
   textSize(60);
-  textFont('Helvetica');
-  text(line2, width/3, height-height/1.5, 600, 200); // Text wraps within text box
+  text(line2, width/3, height-height/1.5, 600, 200);
 
-  fill(50);
-  textSize(60);
-  textFont('Helvetica');
-  text(line3, width/2, height-height/2, 600, 300); // Text wraps within text box
+  text(line3, width/2, height-height/2, 600, 300);
 
-  fill(50);
-  textSize(70);
-  textFont('Helvetica');
-  text(line4, width-(width/2.5), height-height/3, 400, 400); // Text wraps within text box
+  var xPos = width - (width/2.5);
+  var yPos = height-height/2.8;
+
+
+
+  for (i=0; i<line4.length; i++){
+    textSize(70);
+    text(line4[i], xPos+i*45, yPos+i*25, 200, 200);
+  }
 
   noLoop();
 }
