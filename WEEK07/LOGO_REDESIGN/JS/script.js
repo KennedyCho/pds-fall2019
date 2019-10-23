9ecc02438f09f9d2458560dd9bd119a961e0c49b
Kennedy Cambra-Cho <kennedy.cambracho@gmail.com>
2019-10-23 22:40:20 +0800

    logo design

--- WEEK07/LOGO_REDESIGN/JS/script.js
+++ WEEK07/LOGO_REDESIGN/JS/script.js
@@ -1,21 +1,19 @@
+let canvSize = 600;
 function setup() {
-  createCanvas(500, 500);
-  background(220);
-  circGrid(3);
 
+  createCanvas(canvSize, canvSize);
+  background('grey');
+
+  fill(0);
+  // ellipse(canvSize/2, canvSize/2, canvSize*0.7);
+
+  beginShape();
+  vertex(canvSize/5, canvSize/6);
+  bezierVertex(canvSize/9, canvSize-canvSize/20, canvSize-canvSize/9, canvSize-(canvSize/20), canvSize-canvSize/5, canvSize/6);
+  endShape();
 }
 
-function circGrid(numOfCirc) {
-  translate(width/5, 0);
-  for (i=0; i < numOfCirc; i++){
-    console.log(i);
-
-    fill('red');
-    ellipse(i*120, height/2, 100);
-  }
-}
 
 
 function draw() {
-
-}
+  
