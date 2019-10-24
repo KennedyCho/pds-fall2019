70305bbe11a623c08d3941f4fb992e01d311229a
Kennedy Cambra-Cho <kennedy.cambracho@gmail.com>
2019-10-24 13:28:12 +0800

    added diamond patch

--- WEEK07/LOGO_REDESIGN/JS/script.js
+++ WEEK07/LOGO_REDESIGN/JS/script.js
@@ -7,13 +7,24 @@ function setup() {
   fill(0);
   // ellipse(canvSize/2, canvSize/2, canvSize*0.7);
 
-  beginShape();
-  vertex(canvSize/5, canvSize/6);
-  bezierVertex(canvSize/9, canvSize-canvSize/20, canvSize-canvSize/9, canvSize-(canvSize/20), canvSize-canvSize/5, canvSize/6);
-  endShape();
+  // beginShape();
+  // vertex(canvSize/4.5, canvSize/6);
+  // bezierVertex(canvSize/6, canvSize-(canvSize*0.001), canvSize-canvSize/6, canvSize-(canvSize*0.001), canvSize-canvSize/4.5, canvSize/6);
+  // endShape();
+  // angleMode(DEGREES);
+  // translate(-canvSize*0.6/2, -canvSize*0.6/2);
+  // rotate(10);
+  // rect(canvSize/2, canvSize/2, canvSize*0.6, canvSize*0.6, 20);
+
+  angleMode(DEGREES);
+  rotate(PI);
+  translate(-canvSize*0.6/2, -canvSize*0.6/2);
+
+  rect(canvSize/2,canvSize/2, canvSize*0.6, canvSize*0.6, 20);
 }
 
 
 
 function draw() {
-  
+
+}
