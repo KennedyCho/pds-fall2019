cad0edf899868e170ecf94bb6833edd35ce9fa71
Kennedy Cambra-Cho <kennedy.cambracho@gmail.com>
2019-11-04 17:45:54 +0800

    add exercises #4 - #6

--- WEEK09/IN-CLASS/RANDOM01/JS/script.js
+++ WEEK09/IN-CLASS/RANDOM01/JS/script.js
@@ -87,44 +87,97 @@
 //   }
 // }
 
-// EXAMPLE 04: PERLIN NOISE
+// // EXAMPLE 04: PERLIN NOISE
+// function setup() {
+//
+//   createCanvas(600, 600);
+//   background(30);
+//   noFill();
+//
+//   // random()
+//   // -------------------------------------------------------
+//
+//   stroke(255);
+//   strokeWeight(2);
+//   translate(0, 100);
+//   beginShape();
+//   for(let x = 0; x < width; x += 15) {
+//     vertex(x, random(100));
+//   }
+//   endShape();
+//
+//   // noise()
+//   // -------------------------------------------------------
+//
+//   noiseStep = 0;
+//
+//   // To make the noise look the same on each run
+//   // noiseSeed(1);
+//
+//   // To control the curves of the noise
+//   // noiseDetail(1)
+//
+//   translate(0, 200);
+//   beginShape();
+//   for(let x = 0; x < width; x += 15) {
+//     noiseStep += 0.05;
+//     let y = map(noise(noiseStep),0,1,0,200);
+//     vertex(x, y);
+//
+//   }
+//   endShape();
+//
+// }
+
+
+// // EXAMPLE 05: PERLIN NOISE ANIMATED ICEBERG
+// function setup() {
+//
+//   createCanvas(600, 600);
+//   background(30);
+//
+//   noiseDetail(4, 0.75);
+//   noiseSeed(3);
+//
+//   var noiseStep = 0;
+//
+//   translate(width/2, 100);
+//   beginShape();
+//
+//   for(var degree = 0; degree <= 180; degree += 3) {
+//
+//     var x = Math.cos(radians(degree)) * 200;
+//     var y = Math.sin(radians(degree)) * 200;
+//
+//     if(degree > 10 && degree < 170) {
+//       y += noise(noiseStep) * 150;
+//     }
+//
+//     vertex(x, y);
+//
+//     noiseStep += 0.04;
+//   }
+//
+//   endShape();
+// }
+
+
+// EXAMPLE 06: RECREATE DAMIEN HIRST
+
 function setup() {
+  //noprotect
+  createCanvas(400, 400);
+	stroke(255);
+	fill(30);
 
-  createCanvas(600, 600);
-  background(30);
-  noFill();
-
-  // random()
-  // -------------------------------------------------------
-
-  stroke(255);
-  strokeWeight(2);
-  translate(0, 100);
-  beginShape();
-  for(let x = 0; x < width; x += 15) {
-    vertex(x, random(100));
-  }
-  endShape();
-
-  // noise()
-  // -------------------------------------------------------
-
-  noiseStep = 0;
-
-  // To make the noise look the same on each run
-  // noiseSeed(1);
-
-  // To control the curves of the noise
-  // noiseDetail(1)
-
-  translate(0, 200);
-  beginShape();
-  for(let x = 0; x < width; x += 15) {
-    noiseStep += 0.05;
-    let y = map(noise(noiseStep),0,1,0,200);
-    vertex(x, y);
-
-  }
-  endShape();
+	for(var i = 0; i < width; i++) {
+		for(var j = 0; j < height; j++) {
+			var n = noise(i / 8, j / 3);
+			var gray = map(n, 0, 1, 0, 255);
+      var size = map(n, 0, 1, 10, 30);
+      fill(random(255),random(255),random(255),random(200));
+			ellipse(i * 30, j * 30, size, size);
+		}
+	}
 
 }
