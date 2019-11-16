edb0476a653fe0d276e6d8e4d75d741896030653
Kennedy Cambra-Cho <kennedy.cambracho@gmail.com>
2019-11-16 13:59:02 +0800

    update

--- WEEK10/IN-CLASS/PATTERN/JS/script.js
+++ WEEK10/IN-CLASS/PATTERN/JS/script.js
@@ -1,29 +1,71 @@
-let canvSize = 600;
+// create font variable
+// let robotoLight;
 
-function setup() {
+let x;
+let y;
+let xPadding;
+let yPadding;
+let spacing;
+let xMove;
+let yMove;
+let fontSize;
+let radius;
+let circleSize;
+// < > { }
 
+function preload(){
+//   change value of variable
+  robotoLight = loadFont('ASSETS/Roboto-Thin.ttf');
 
-  createCanvas(canvSize, canvSize);
 }
 
-function draw() {
+function setup() {
+  createCanvas(400, 400);
   background(220);
 
-  for (i=0; i < canvSize; i++){
+  translate(width/2 ,height/2);
 
-    for (j=0; j<canvSize; j++){
+  angleMode(DEGREES);
 
-        noStroke();
-        fill(i);
-        ellipse(i*50, j* 50, 20);
+  xPadding = 30;
+  yPadding = 30;
 
-        noStroke();
-        fill(i*20);
-        ellipse(i*30, j* 50, 20);
-    }
+  x = 0;
+  y = 0;
+
+  spacing = 2;
+
+  xMove = 0;
+  yMove = 0;
+
+  fontSize = 30;
+  radius = width-100;
+
+  circleSize = width * 15;
+
+  textFont(robotoLight);
+  textSize(fontSize);
+  textAlign(CENTER);
+
+
+  drawSemiColon(x, y, radius, spacing, fontSize)
+
+}
+
+
+function drawSemiColon(x, y, radius, spacing, fontSize){
+
+  console.log(cos(x));
+  textSize(fontSize);
+  fill(255-radius);
+  text('O', cos(x) * radius, sin(y)* radius);
+
+  if(x <= circleSize){
+
+    drawSemiColon(x + spacing , y + spacing , radius - 0.1, spacing);
+
 
   }
 
-  noLoop();
 
 }
