6e62b0e0ef9445338d7f7afcb73897c37fa0649b
Kennedy Cambra-Cho <kennedy.cambracho@gmail.com>
2019-11-18 16:52:20 +0800

    change p5 lib location

--- WEEK11/IN-CLASS/PIXEL_EXAMPLE/JS/script.js
+++ WEEK11/IN-CLASS/PIXEL_EXAMPLE/JS/script.js
@@ -1,46 +1,46 @@
 // EXAMPLE 01
 
-function setup() {
-  createCanvas(400, 400);
-  pixelDensity(1);
-  background(30);
-
-  // Draw a single red pixel
-  noStroke();
-  fill(255, 0, 0);
-  rect(180, 200, 10, 10);
-
-  // Find the color of the pixel in the pixels array
-  loadPixels();
-	//            x  +   y  * width   * Vals per color
-  var index = (180 + (200 * width)) * 4;
-  var r = pixels[index + 0];
-  var g = pixels[index + 1];
-  var b = pixels[index + 2];
-  print(r);
-  print(g);
-  print(b);
-}
-
-// EXAMPLE 02
-
 // function setup() {
-//   createCanvas(255, 255);
+//   createCanvas(400, 400);
 //   pixelDensity(1);
 //   background(30);
 //
+//   // Draw a single red pixel
+//   noStroke();
+//   fill(255, 0, 0);
+//   rect(180, 200, 10, 10);
+//
+//   // Find the color of the pixel in the pixels array
 //   loadPixels();
-//
-//   for(var x = 0; x < width; x++) {
-//   	for(var y = 0; y < height; y++) {
-//
-//   		var index = (x + (y * width)) * 4;
-//       pixels[index + 0] = x;
-//   		pixels[index + 1] = y;
-//   		pixels[index + 2] = 0;
-//
-//     }
-//   }
-//
-//   updatePixels();
+// 	//            x  +   y  * width   * Vals per color
+//   var index = (180 + (200 * width)) * 4;
+//   var r = pixels[index + 0];
+//   var g = pixels[index + 1];
+//   var b = pixels[index + 2];
+//   print(r);
+//   print(g);
+//   print(b);
 // }
+
+// EXAMPLE 02
+
+function setup() {
+  createCanvas(255, 255);
+  pixelDensity(1);
+  background(30);
+
+  loadPixels();
+
+  for(var x = 0; x < width; x++) {
+  	for(var y = 0; y < height; y++) {
+
+  		var index = (x + (y * width)) * 4;
+      pixels[index + 0] = x;
+  		pixels[index + 1] = 0;
+  		pixels[index + 2] = y;
+
+    }
+  }
+
+  updatePixels();
+}
